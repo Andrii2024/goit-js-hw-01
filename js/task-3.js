aa39269097207ee638cad72812cf2df6f8dee2ce
Andrii2024 <krovlya2005@gmail.com>
2023-12-15 18:36:50 +0200

    of

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,9 @@
-//  getElementWidth(content, padding, border);
-let content = parseFloat('50px');
-let padding = parseFloat('8px');
-let border = parseFloat('4px');
-let getElementWidth = content + 2 * padding + 2 * border;
+function getElementWidth(content, padding, border) {
+  const getElementWidth =
+    parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
+  return getElementWidth;
+}
 
-console.log(getElementWidth);
-
-// console.log(getElementWidth('50px', '8px', '4px')); // 74
-// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-// console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
